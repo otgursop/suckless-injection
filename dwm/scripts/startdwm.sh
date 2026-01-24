280341810b630924b58a4d72812c6b135f7cdfc3
otgursop <otgursop@git.com>
2026-01-24 15:14:42 +0500

    first commit

---
+++ dwm/scripts/startdwm.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Keyboard layouts
+setxkbmap us,ru -option 'grp:alt_shift_toggle' &
+
+# Wallpapers
+feh --bg-scale ~/Pictures/wallpapers/wallpaper23.png
+
+# Tray
+flameshot &
+nm-applet &
+blueman-applet &
+
+# Notifications
+dunst &
+
+# Status bar
+dwmblocks &
+
+# Mouse settings
+# Turn off the acceleration
+(sleep 2 && \
+for id in $(xinput list | grep -i pointer | sed 's/.*id=\([0-9]*\).*/\1/'); do
+    xinput set-prop $id "libinput Accel Profile Enabled" 0, 1 2>/dev/null
+    xinput set-prop $id "libinput Accel Speed" 0 2>/dev/null
+done && \
+xset m 0 0 2>/dev/null) &
+
+# DWM
+while true; do
+    dwm 2> ~/.dwm.log
+done
---
+++ dwmblocks-async/config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// String used to delimit block outputs in the status.
+#define DELIMITER " / "
+
+// Maximum number of Unicode characters that a block can output.
+#define MAX_BLOCK_OUTPUT_LENGTH 45
+
+// Control whether blocks are clickable.
+#define CLICKABLE_BLOCKS 0
+
+// Control whether a leading delimiter should be prepended to the status.
+#define LEADING_DELIMITER 0
+
+// Control whether a trailing delimiter should be appended to the status.
+#define TRAILING_DELIMITER 0
+
+// Define blocks for the status feed as X(icon, cmd, interval, signal).
+#define BLOCKS(X) \
+    X("| 🌐 ", "echo $(xset -q|grep LED| awk '{ if (substr($10,5,1) == 1)  print \"RU\"; else print \"EN\"; }')", 1, 5) \
+    X("", "amixer get Master | grep -q \"\\[off\\]\" && echo \" \" || amixer get Master | awk -F\"[][]\" '/%/ {printf \"  %s\", $2; exit}'", 1, 2) \
+    X(" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 9) \
+    X("", "echo $(date '+󰃰 %d.%m -  %H:%M')\" |\"", 5, 0)
+
+#endif  // CONFIG_H
